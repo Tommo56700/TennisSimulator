b059ecb516ca25ad7b2328284ac85e31c3e9883e
Tommo56700 <sean.wtomlinson@talktalk.net>
2021-06-25 10:09:25 +0100

    Set up initial structure for the game simulator

--- TennisSimulator/TennisSimulator/Program.cs
+++ TennisSimulator/TennisSimulator/Program.cs
@@ -6,7 +6,46 @@ namespace TennisSimulator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+        }
+    }
+
+    public class GameScore
+    {
+        public GameScore(int player1Score = 0, int player2Score = 0)
+        {
+            Player1Score = player1Score;
+            Player2Score = player2Score;
+        }
+
+        public int Player1Score { get; set; }
+        public int Player2Score { get; set; }
+    }
+
+    public class GameSimulator
+    {
+        public GameScore SimulateGame()
+        {
+            var gameScore = new GameScore();
+
+            while (!IsGameOver(gameScore))
+            {
+                
+            }
+
+            return gameScore;
+        }
+
+        private bool IsGameOver(GameScore gameScore) => ;
+
+        private void RandomlyAwardPoint(GameScore gameScore)
+        {
+            var random = new Random();
+
+            if (random.NextDouble() > 0.5)
+                gameScore.Player1Score++;
+            else
+                gameScore.Player2Score++;
         }
     }
 }
